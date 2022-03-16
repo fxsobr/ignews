5daa4a971cea35ab03e23a7ad9b84ee5db3224a4
Marcelo Ceolin <mceolin@unidavi.edu.br>
2022-03-16 09:09:46 -0300

    criado estilos globais da aplicação

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <title>ig.news</title>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700;1,900&display=swap" rel="stylesheet" />
