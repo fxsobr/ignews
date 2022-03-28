83aeab4068343f3ecf3405022265990a0527e4f0
Marcelo Ceolin <mceolin@unidavi.edu.br>
2022-03-28 17:58:48 -0300

    criado pagina de post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,11 @@
 import { GetStaticProps } from 'next';
-import { Head } from 'next/document';
 import * as prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import Head from 'next/head';
 
 type Post = {
     slug: string,
@@ -20,14 +21,19 @@ interface PostsProps {
 export default function Posts({ posts }: PostsProps) {
     return (
         <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                        <a key={post.slug} href="#">
-                            <time>{post.updatedAt}</time>
-                            <strong>{post.title}</strong>
-                            <p>{post.excerpt}</p>
-                        </a>
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
                     ))}
                 </div>
             </main>
